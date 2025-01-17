18b32ebeafb076058071f7d4e7d477529171ed39
shivendram16053 <shivendramishra.sm93@gmail.com>
2025-01-17 12:04:21 +0530

    Class 3rd

--- solana-starter/ts/cluster1/nft_image.ts
+++ solana-starter/ts/cluster1/nft_image.ts
@@ -1,8 +1,9 @@
-import wallet from "../wba-wallet.json"
+import wallet from "../../../Turbin3_wallet.json"
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
 import { createGenericFile, createSignerFromKeypair, signerIdentity } from "@metaplex-foundation/umi"
 import { irysUploader } from "@metaplex-foundation/umi-uploader-irys"
 import { readFile } from "fs/promises"
+import { read } from "fs"
 
 // Create a devnet connection
 const umi = createUmi('https://api.devnet.solana.com');
@@ -10,19 +11,25 @@ const umi = createUmi('https://api.devnet.solana.com');
 let keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
 const signer = createSignerFromKeypair(umi, keypair);
 
-umi.use(irysUploader());
+umi.use(irysUploader({address:"https://devnet.irys.xyz"}));
 umi.use(signerIdentity(signer));
 
 (async () => {
     try {
         //1. Load image
+        const image = await readFile("../young_jeff.jpg")
         //2. Convert image to generic file.
+        const file = createGenericFile(image,
+            "shivendrarug.png",
+            {
+                contentType:"image/png"
+            }
+        )
         //3. Upload image
 
-        // const image = ???
 
-        // const [myUri] = ??? 
-        // console.log("Your image URI: ", myUri);
+        const [myUri] = await umi.uploader.upload([file]) 
+        console.log("Your image URI: ", myUri);
     }
     catch(error) {
         console.log("Oops.. Something went wrong", error);
